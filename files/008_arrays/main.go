7c8f15ff649a752f97aaf3ed32b657c47a2d6fb8
enkhee-Osiris <enkhee.ag@gmail.com>
2026-05-18 13:55:53 +0800

    add: array codes

---
+++ files/008_arrays/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a [5]int
+	fmt.Println("empty:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("decimal:", b)
+
+	b = [...]int{1, 1, 1, 1, 1}
+	fmt.Println("decimal:", b)
+
+	b = [...]int{100, 3: 400, 500}
+	fmt.Println("idx:", b)
+
+	var twoD [2][3]int
+	for i := range 2 {
+		for j := range 3 {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
+
+	twoD = [2][3]int{
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	fmt.Println("2d: ", twoD)
+}
